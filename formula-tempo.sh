925565e55e776afc952e91b44a4b8e001934022f
rubervalrss <rubervalrss@gmail.com>
2020-11-22 20:04:14 -0300

    Adicionado o arquivo formula-tempo.sh

---
+++ formula-tempo.sh
@@ -0,0 +1,23 @@
+“#!/bin/bash
+clear;
+echo"Formula do Tempo";
+<!DOCTYPE html>
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=uft-8">
+<body>
+
+	<h1 style="text-aling:center;">Bem-vindo(a) a Formula do Tempo!</h1>
+
+	<h2 style="text-aling:center;"><p>Gestão de Tempo com foco em  você!</h2>
+ 
+		Quando  falamos em gestão de tempo, estamos falando em priorizar,
+		 o que precisa ser feito para gerenciar: O nosso tempo A nossa vida... </P>
+	<center>
+		<!-- <a href="http://www.udemy.com/user/rubervalrss/"> -->
+		<a href="https://github.com/rubervalrss/" target="black">
+		<img src="images/perfil.jpg">
+		</a>
+	</center>
+</body>
+</html>;
+exit;
